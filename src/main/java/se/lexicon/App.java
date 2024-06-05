b1b004031b7b50be0daa706f85e84d4111eba2e8
Mohammed Dawood <m.dawood.780909@gmail.com>
2024-06-05 11:43:43 +0200

    Update code

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -3,6 +3,6 @@ package se.lexicon;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Mohammed!");
+        System.out.println("Mohammed");
     }
 }
