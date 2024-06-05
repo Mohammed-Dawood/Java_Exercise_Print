cc0d2e84ca11ed6b1fdc6ad2e7df3fe2ce34b4db
Mohammed Dawood <m.dawood.780909@gmail.com>
2024-06-05 11:42:23 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,14 @@
+package se.lexicon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello");
+        System.out.println("Mohammed!");
+    }
+}
